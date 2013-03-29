4185f3f69c44826323877b1535554e08dd96f221
ys08004 <odai3333@yomiuri.com>
2013-03-29 14:46:37 +0900

    アカウント情報記憶しておくのをやめる

--- spec/pcoder_spec.rb
+++ spec/pcoder_spec.rb
@@ -7,30 +7,7 @@ module Pcoder
   describe Atcoder do
     let(:atcoder) { Atcoder.new() }
 
-    describe "#account" do
-      it "ユーザ名とパスワードが保存されること" do
-        user, password = nil
-        atcoder.account("user", "password", "spec_account.conf")
-        File::open("conf/spec_account.conf") { |line|
-          user = line[0]
-          password = line[1]
-        }
-        user.should eq "user"
-        password.should eq "password"
-      end
-    end
-
-    describe "#get_user" do
-      it "ユーザ名を出力すること" do
-        pending
-      end
-    end
-
     describe "#login" do
-      it "ユーザ名かパスワードがない場合はエラーを投げること" do
-        pending
-      end
-
       it "ログインに失敗したらfalseを返すこと" do
         pending
       end
