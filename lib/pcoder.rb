1d2c402bbf8519098839e1ef0b1256341b3cc027
ys08004 <odai3333@yomiuri.com>
2013-03-29 16:48:38 +0900

    Added Atcoder#submit

--- lib/pcoder.rb
+++ lib/pcoder.rb
@@ -1,6 +1,33 @@
+require 'mechanize'
+
 module Pcoder
-  require 'mechanize'
+  ATCODER_URI = "contest.atcoder.jp"
 
   class Atcoder
+    def submit(user, pass, file)
+      contest, assignment, extension = file.split(/[_.]/)
+      uri = "http://#{contest}.#{ATCODER_URI}"
+      agent = login(user, pass, uri)
+      raise "LoginError" if agent.nil?
+      agent.get("#{uri}/submit")
+      agent.page.form_with do |f|
+        f.field_with(:name => "name").value = user
+        f.field_with(:name => "password").value = pass
+      end.click_button
+    end
+
+    def login(user, pass, uri)
+      agent = Mechanize.new
+      agent.get("#{uri}/login")
+      before_uri = agent.page.uri
+      agent.page.form_with do |f|
+        f.field_with(:name => "name").value = user
+        f.field_with(:name => "password").value = pass
+      end.click_button
+      return nil if agent.page.uri == before_uri
+      agent
+    end
   end
 end
+
+Pcoder::Atcoder.new.submit("user","pass","arc012.rb")
--- spec/pcoder_spec.rb
+++ spec/pcoder_spec.rb
@@ -7,17 +7,7 @@ module Pcoder
   describe Atcoder do
     let(:atcoder) { Atcoder.new() }
 
-    describe "#login" do
-      it "ログインに失敗したらfalseを返すこと" do
-        pending
-      end
-
-      it "ログインに成功したらtrueを返すこと" do
-        pending
-      end
-    end
-
-    describe "#post_code" do
+    describe "#submit" do
       it "コードが提出されること" do
         pending
       end
